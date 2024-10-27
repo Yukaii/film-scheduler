4a6e46261aaea843da0bd668ea97f8276c0018b8
Yukai Huang <yukaihuangtw@gmail.com>
2024-10-27 15:51:13 +0800

    fix: adjust mini preview color

--- apps/film-scheduler/components/SessionsMiniPreview.tsx
+++ apps/film-scheduler/components/SessionsMiniPreview.tsx
@@ -1,6 +1,5 @@
-import React, { useMemo } from 'react';
-import clsx from 'clsx';
-import dayjs from 'dayjs';
+import React, { useMemo } from "react";
+import dayjs from "dayjs";
 import {
   TooltipTrigger,
   TooltipProvider,
@@ -8,7 +7,7 @@ import {
   TooltipContent,
 } from "@/components/ui/tooltip";
 import { useAppContext } from "@/contexts/AppContext";
-import { generateSessionId } from "@/lib/utils";
+import { generateSessionId, cn } from "@/lib/utils";
 import { Session, Film } from "@/components/types";
 
 interface MonthViewProps {
@@ -17,7 +16,7 @@ interface MonthViewProps {
   onSelectSession: (sessionId: string) => void;
 }
 
-type SessionWithId = Session & { id: string, film: Film };
+type SessionWithId = Session & { id: string; film: Film };
 interface WeekWithSessions {
   days: {
     day: dayjs.Dayjs;
@@ -26,50 +25,59 @@ interface WeekWithSessions {
   }[];
 }
 
-export const SessionsMiniPreview: React.FC<MonthViewProps> = ({ sessions, selectedSessionIds, onSelectSession }) => {
-  const {
-    filmsMap,
-  } = useAppContext();
+export const SessionsMiniPreview: React.FC<MonthViewProps> = ({
+  sessions,
+  selectedSessionIds,
+  onSelectSession,
+}) => {
+  const { filmsMap } = useAppContext();
 
   const weeksWithSessions = useMemo(() => {
     const sortedSession = sessions.sort((a, b) => a.time - b.time);
     const firstSession = sortedSession.at(0);
     const lastSession = sortedSession.at(-1);
-    const firstDisplayDay = dayjs(firstSession?.time).startOf('week');
-    const lastDisplayDay = dayjs(lastSession?.time).endOf('week');
+    const firstDisplayDay = dayjs(firstSession?.time).startOf("week");
+    const lastDisplayDay = dayjs(lastSession?.time).endOf("week");
 
-    const sessionsByDate = sessions.reduce<Record<string, SessionWithId[]>>((acc, session) => {
-      const dateKey = dayjs(session.time).format('YYYY-MM-DD');
-      if (!acc[dateKey]) {
-        acc[dateKey] = [];
-      }
-      acc[dateKey].push({
-        ...session,
-        film: filmsMap.get(session.filmId)!,
-        id: generateSessionId(session),
-       });
-      return acc;
-    }, {});
+    const sessionsByDate = sessions.reduce<Record<string, SessionWithId[]>>(
+      (acc, session) => {
+        const dateKey = dayjs(session.time).format("YYYY-MM-DD");
+        if (!acc[dateKey]) {
+          acc[dateKey] = [];
+        }
+        acc[dateKey].push({
+          ...session,
+          film: filmsMap.get(session.filmId)!,
+          id: generateSessionId(session),
+        });
+        return acc;
+      },
+      {},
+    );
 
-    return Array.from<unknown, WeekWithSessions>({ length: lastDisplayDay.diff(firstDisplayDay, 'week') + 1 }, (_, i) => {
-      const firstWeekDay = firstDisplayDay.add(i, 'week');
-      return {
-        days: Array.from({ length: 7 }, (_, j) => {
-          const day = firstWeekDay.add(j, 'day');
-          return {
-            day,
-            showMonth: (i === 0 && j === 0) || day.isSame(day.startOf('month')),
-            sessions: sessionsByDate[day.format('YYYY-MM-DD')] ?? []
-          };
-        }),
-      };
-    });
+    return Array.from<unknown, WeekWithSessions>(
+      { length: lastDisplayDay.diff(firstDisplayDay, "week") + 1 },
+      (_, i) => {
+        const firstWeekDay = firstDisplayDay.add(i, "week");
+        return {
+          days: Array.from({ length: 7 }, (_, j) => {
+            const day = firstWeekDay.add(j, "day");
+            return {
+              day,
+              showMonth:
+                (i === 0 && j === 0) || day.isSame(day.startOf("month")),
+              sessions: sessionsByDate[day.format("YYYY-MM-DD")] ?? [],
+            };
+          }),
+        };
+      },
+    );
   }, [sessions, filmsMap]);
 
   return (
     <div className="max-w-4xl mx-auto p-4">
       <div className="grid grid-cols-7 gap-4 mb-4">
-        {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
+        {["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"].map((day) => (
           <div key={day} className="font-bold text-center">
             {day}
           </div>
@@ -78,30 +86,35 @@ export const SessionsMiniPreview: React.FC<MonthViewProps> = ({ sessions, select
       {weeksWithSessions.map((week, weekIdx) => (
         <div key={`week-${weekIdx}`} className="grid grid-cols-7 gap-4 mb-4">
           {week.days.map(({ day, sessions, showMonth }) => (
-            <div key={day.format('YYYY-MM-DD')} className="border border-gray-200 p-1">
-              { showMonth && (
-                <div className="text-xs">{day.format('MMM')}</div>
-              ) }
-              <div className="font-bold text-sm mb-1">{day.format('D')}</div>
-              {sessions.map((session) => (
-                <TooltipProvider key={session.id} delayDuration={0}>
-                  <Tooltip>
-                    <TooltipTrigger asChild>
-                      <div
-                        onClick={() => onSelectSession(session.id)}
-                        className={clsx(
-                          "h-4 rounded mb-1 cursor-pointer",
-                          selectedSessionIds.has(session.id) ? 'bg-violet-900' : 'bg-slate-800'
-                        )}
-                      />
-                    </TooltipTrigger>
-                    <TooltipContent side='bottom'>
-                      {session.film.filmTitle} {session.location} -{" "}
-                      {new Date(session.time).toLocaleString()}
-                    </TooltipContent>
-                  </Tooltip>
-                </TooltipProvider>
-              ))}
+            <div
+              key={day.format("YYYY-MM-DD")}
+              className="border border-gray-200 p-1"
+            >
+              {showMonth && <div className="text-xs">{day.format("MMM")}</div>}
+              <div className="font-bold text-sm mb-1">{day.format("D")}</div>
+              {sessions.map((session) => {
+                const isSelected = selectedSessionIds.has(session.id);
+
+                return (
+                  <TooltipProvider key={session.id} delayDuration={0}>
+                    <Tooltip>
+                      <TooltipTrigger asChild>
+                        <div
+                          onClick={() => onSelectSession(session.id)}
+                          className={cn("h-4 rounded mb-1 cursor-pointer", {
+                            "dark:bg-violet-500 bg-violet-700": isSelected,
+                            "dark:bg-slate-600 bg-slate-300": !isSelected,
+                          })}
+                        />
+                      </TooltipTrigger>
+                      <TooltipContent side="bottom">
+                        {session.film.filmTitle} {session.location} -{" "}
+                        {new Date(session.time).toLocaleString()}
+                      </TooltipContent>
+                    </Tooltip>
+                  </TooltipProvider>
+                );
+              })}
             </div>
           ))}
         </div>
