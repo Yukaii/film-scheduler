9a949ed56a3a0ec4a96d6dc3a779da2d29a41e13
Yukai Huang <yukaihuangtw@gmail.com>
2025-03-20 23:34:44 +0800

    fix: correct import paths for JSON files in data module

--- packages/film-source-golden-horse/src/data/index.ts
+++ packages/film-source-golden-horse/src/data/index.ts
@@ -1,8 +1,8 @@
 // Import JSON files directly as data
-import filmDetailsCache from '../film_details_cache.json';
-import filmListCache from '../film_list_cache.json';
-import filmSectionsMap from '../film_sections_map.json';
-import sectionsCache from '../sections_cache.json';
+import filmDetailsCache from '../../film_details_cache.json';
+import filmListCache from '../../film_list_cache.json';
+import filmSectionsMap from '../../film_sections_map.json';
+import sectionsCache from '../../sections_cache.json';
 
 export {
   filmDetailsCache,
